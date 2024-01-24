a3749f7f0ebfb694cb90f387566486fd764ffa8f
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 23:03:27 +0800

    pseudocode

---
+++ exponential.py
@@ -0,0 +1,15 @@
+# Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# pseudocode
+
+# create a function called exponent 
+
+# calculate the result when the base is raise to exp
+
+# conditions
+   # check if the exp is equal to 0
+
+   # check if the exp is less than 0
+
+# print the result
